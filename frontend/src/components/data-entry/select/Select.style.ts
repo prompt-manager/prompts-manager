5bd2b7c6745efc8465f455293d78f4f80ee37df7
jieun <jeahn@nkia.co.kr>
2025-07-23 14:09:05 +0900

    Styles: Input, Select, error state 스타일 추가

--- frontend/src/components/data-entry/select/Select.style.ts
+++ frontend/src/components/data-entry/select/Select.style.ts
@@ -3,44 +3,49 @@ import styled from 'styled-components'
 import { ExtendedSelectProps } from './Select'
 
 export const S_Select = styled(Select)<ExtendedSelectProps>`
-    width: ${({ width }) => width} !important;
+  width: ${({ width }) => width} !important;
 
-    && .ant-select-selector {
-        border: 1px solid var(--green-z-01);
-        background: var(--background);
-        color: var(--white);
+  && .ant-select-selector {
+    border: 1px solid var(--green-z-01);
+    background: var(--background);
+    color: var(--white);
+  }
+
+  &&:hover .ant-select-selector,
+  &&.ant-select-focused .ant-select-selector,
+  &&.ant-select-open .ant-select-selector {
+    border-color: var(--yellow-z-01) !important;
+    box-shadow: none !important;
+  }
+
+  && .ant-select-selection-item {
+    color: var(--white);
+  }
+
+  && .ant-select-selection-placeholder {
+    color: var(--text-placeholder);
+  }
+
+  &&.ant-select-disabled .ant-select-selector {
+    background-color: var(--disabled-input);
+    color: var(--text-disabled);
+
+    &:hover {
+      border-color: var(--green-z-01);
     }
+  }
 
-    &&:hover .ant-select-selector,
-    &&.ant-select-focused .ant-select-selector,
-    &&.ant-select-open .ant-select-selector {
-        border-color: var(--yellow-z-01) !important;
-        box-shadow: none !important;
-    }
+  &&.ant-select-disabled .ant-select-selection-placeholder {
+    color: var(--text-disabled);
+  }
 
-    && .ant-select-selection-item {
-        color: var(--white);
-    }
+  && .ant-select-arrow {
+    color: var(--green-z-01);
+  }
 
-    && .ant-select-selection-placeholder {
-        color: var(--text-placeholder);
-    }
-
-    &&.ant-select-disabled .ant-select-selector {
-        background-color: var(--disabled-input);
-        color: var(--text-disabled);
-
-        &:hover {
-            border-color: var(--green-z-01);
-        }
-    }
-
-    &&.ant-select-disabled .ant-select-selection-placeholder {
-        color: var(--text-disabled);
-    }
-
-    && .ant-select-arrow {
-        color: var(--green-z-01);
-    }
+  &&.ant-select-status-error .ant-select-selector {
+    background-color: transparent;
+    border-color: var(--border-error);
+  }
 `
 S_Select.displayName = 'S_Select'
