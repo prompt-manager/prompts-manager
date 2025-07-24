195f2e3a22d7c749eeffe980b298e46250f2e76e
jieun <jeahn@nkia.co.kr>
2025-07-24 14:52:30 +0900

    Feat: prompt 관리 관련 api, 타입 정의

--- frontend/src/api/service/apiEndpoint.ts
+++ frontend/src/api/service/apiEndpoint.ts
@@ -1,6 +1,9 @@
 const BASE_URL = process.env.REACT_APP_DEV_PROXY_SERVER
 export const PROMPT_ENDPOINT = `${BASE_URL}/prompts`
-export const PROMPT_NODE = `${PROMPT_ENDPOINT}/nodes`
+export const PROMPT_NODE = `${PROMPT_ENDPOINT}/node`
+export const PROMPT_NODES = `${PROMPT_ENDPOINT}/nodes`
+export const PROMPT_NODES_SUMMARY = `${PROMPT_ENDPOINT}/nodes-summary`
+export const PROMPTS_DELETE_ALL = `${PROMPT_ENDPOINT}/delete-all`
 
 export const DATASETS_ENDPOINT = `${BASE_URL}/datasets/`
 export const CHAINS_ENDPOINT = `${BASE_URL}/chains`
