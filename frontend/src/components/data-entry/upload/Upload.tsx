a421c5c6a9403284be28299767ca6be383c5ee04
jieun <jeahn@nkia.co.kr>
2025-08-07 11:01:54 +0900

    Styles: 업로드 컴포넌트 스타일 수정

--- frontend/src/components/data-entry/upload/Upload.tsx
+++ frontend/src/components/data-entry/upload/Upload.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-import S_Upload from './Upload.style'
+import React, { useState } from 'react'
+import { S_Upload, S_UploadContainer } from './Upload.style'
 import { UploadProps } from 'antd'
+import { PlusCircleOutlined, PaperClipOutlined } from '@ant-design/icons'
 
 const Upload = ({ ...props }: UploadProps) => {
-    return <S_Upload {...props}>{props.children}</S_Upload>
+  const [fileList, setFileList] = useState<any>([])
+
+  const handleChange = ({ fileList: newFileList }: { fileList: any }) => {
+    setFileList(newFileList)
+  }
+
+  return (
+    <S_Upload {...props} fileList={fileList} onChange={handleChange}>
+      {fileList.length === 0 && (
+        <S_UploadContainer>
+          <PlusCircleOutlined />
+        </S_UploadContainer>
+      )}
+    </S_Upload>
+  )
 }
 
 export default Upload
